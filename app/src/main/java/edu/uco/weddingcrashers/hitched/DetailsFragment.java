caa2e17a064e4c744a8e4478d268f33fb3c10f54
Tung Nguyen <ngoctung46@gmail.com>
2016-02-04 15:00:13 -0600

    Create Details Fragment for vendors. Using SingleFragmentActivity as a template for the whole fragments in my part. Implemented the use of recycle view to display the list of vendors.

---
+++ app/src/main/java/edu/uco/weddingcrashers/hitched/DetailsFragment.java
@@ -0,0 +1,79 @@
+package edu.uco.weddingcrashers.hitched;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Tung Nguyen on 2/4/2016.
+ */
+public class DetailsFragment extends Fragment {
+
+    private RecyclerView mVendorRecycleView;
+    private Vendor mVendor;
+    private VendorAdapter mAdapter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mVendor = new Vendor();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_detail,container,false);
+        mVendorRecycleView = (RecyclerView) view.findViewById(R.id.detail_vendor_recycle_view);
+        mVendorRecycleView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        return view;
+    }
+
+    private void updateUI(){
+        VendorList vendorList = VendorList.get(getActivity());
+        List<Vendor> vendors = vendorList.getVendors();
+
+        mAdapter = new VendorAdapter(vendors);
+        mVendorRecycleView.setAdapter(mAdapter);
+    }
+
+    private class VendorHolder extends RecyclerView.ViewHolder{
+        public TextView mTextView;
+        public VendorHolder(View itemView) {
+            super(itemView);
+            mTextView = (TextView)itemView;
+        }
+    }
+
+    private class VendorAdapter extends RecyclerView.Adapter<VendorHolder>{
+        private List<Vendor> mVendors;
+        public VendorAdapter(List<Vendor> vendors){
+            mVendors = vendors;
+        }
+
+        @Override
+        public VendorHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            View view = layoutInflater.inflate(android.R.layout.simple_list_item_1,viewGroup,false);
+            return new VendorHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(VendorHolder vendorHolder, int i) {
+            Vendor vendor = mVendors.get(i);
+            vendorHolder.mTextView.setText(vendor.getVendorName());
+        }
+
+        @Override
+        public int getItemCount() {
+            return mVendors.size();
+        }
+    }
+}
