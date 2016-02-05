54b14283e43dfaf70b8eda8e454894d0fe8f480b
summitghimire <sghimire7@uco.edu>
2016-02-05 16:08:56 -0600

    Added Pictures Class

---
+++ app/src/main/java/edu/uco/weddingcrashers/hitched/Pictures.java
@@ -0,0 +1,14 @@
+package edu.uco.weddingcrashers.hitched;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Pictures extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pictures);
+    }
+
+}
