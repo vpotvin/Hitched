e79d7498f56343e0051463db29b74a21ec657a7e
Tung Nguyen <ngoctung46@gmail.com>
2016-02-11 14:37:22 -0600

    Modify vendor list

--- app/src/main/java/edu/uco/weddingcrashers/hitched/DetailsFragment.java
+++ app/src/main/java/edu/uco/weddingcrashers/hitched/DetailsFragment.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -46,21 +47,31 @@ public class DetailsFragment extends Fragment {
     }
 
     private class VendorHolder extends RecyclerView.ViewHolder{
+        private Vendor mVendor;
         public TextView mTextView;
+        public ImageView mImageView;
         public VendorHolder(View itemView) {
             super(itemView);
-            mTextView = (TextView)itemView;
+            mTextView = (TextView)itemView.findViewById(R.id.list_item_vendor_name_text_view);
+            mImageView = (ImageView)itemView.findViewById(R.id.vendorListImageView);
+        }
+        public void bindVendor(Vendor vendor){
+            mVendor = vendor;
+            mTextView.setText(mVendor.getVendorName());
+            mImageView.setImageResource(R.drawable.a);
         }
     }
 
     private class VendorAdapter extends RecyclerView.Adapter<VendorHolder>{
         private List<Vendor> mVendors;
+
         public VendorAdapter(List<Vendor> vendors){
             mVendors = vendors;
         }
 
         @Override
         public VendorHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+
             LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
             View view = layoutInflater.inflate(R.layout.list_vendor_details,viewGroup,false);
             return new VendorHolder(view);
@@ -69,12 +80,14 @@ public class DetailsFragment extends Fragment {
         @Override
         public void onBindViewHolder(VendorHolder vendorHolder, int i) {
             Vendor vendor = mVendors.get(i);
-            vendorHolder.mTextView.setText(vendor.getVendorName());
+            vendorHolder.bindVendor(vendor);
         }
 
         @Override
         public int getItemCount() {
             return mVendors.size();
         }
+
     }
+
 }
