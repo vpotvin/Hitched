05321015bb7df2d0c4e51bb3b6b5ff4f2d53972f
vpotvin <vdpotvin@gmail.com>
2016-02-18 20:15:07 -0600

    Setup to work on itinerary

---
+++ app/src/main/java/edu/uco/weddingcrashers/hitched/ItineraryItem.java
@@ -0,0 +1,19 @@
+package edu.uco.weddingcrashers.hitched;
+
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+
+/**
+ * Created by vdpotvin on 2/18/16.
+ */
+
+@ParseClassName("ItineraryItem")
+public class ItineraryItem extends ParseObject {
+    public String getTitle() {
+        return getString("title");
+    }
+
+    public void setTitle(String title) {
+        put("title", title);
+    }
+}
