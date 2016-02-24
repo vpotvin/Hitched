4c1cb71a55dd9c0a36cc5010560e089a1b77c136
Victoria Potvin <vdpotvin@gmail.com>
2016-02-23 19:07:10 -0600

    itinerary basic functions working

--- app/src/main/java/edu/uco/weddingcrashers/hitched/ItineraryItem.java
+++ app/src/main/java/edu/uco/weddingcrashers/hitched/ItineraryItem.java
@@ -3,6 +3,8 @@ package edu.uco.weddingcrashers.hitched;
 import com.parse.ParseClassName;
 import com.parse.ParseObject;
 
+import java.util.Date;
+
 /**
  * Created by vdpotvin on 2/18/16.
  *
@@ -26,4 +28,22 @@ public class ItineraryItem extends ParseObject {
     public void setAssigned(String assigned) {
         put("assigned", assigned);
     }
+
+    public void setTime(Date date) {
+        put("time", date);
+    }
+
+    public Date getTime() {
+        return getDate("time");
+    }
+
+    public Double getTip() {
+        return getDouble("tip");
+    }
+
+    public void setTip(double tip) {
+        put("tip", tip);
+    }
+
+
 }
